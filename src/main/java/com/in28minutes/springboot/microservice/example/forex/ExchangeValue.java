2166d6250f3e5a26705e4830693ec70c53997341
John Hou <houqisen@gmail.com>
2018-04-24 10:00:30 -0700

    first commit

---
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/ExchangeValue.java
@@ -0,0 +1,59 @@
+package com.in28minutes.springboot.microservice.example.forex;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+	@Id
+	private Long id;
+	
+	@Column(name="currency_from")
+	private String from;
+	
+	@Column(name="currency_to")
+	private String to;
+	
+	@Column(name="conversion_multiple")
+	private BigDecimal conversionMultiple;
+	
+	private int port;
+	
+	public ExchangeValue() {}
+
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.port = port;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+}
